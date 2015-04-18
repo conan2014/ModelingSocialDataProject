4fb0941076c41e5052c8f4e5d4353cea3e5f0d54
jeffhudson <jdh2182@columbia.edu>
2015-04-17 20:59:56 -0400

    fixed friend count for users with zero friends

--- extract_friend_list.R
+++ extract_friend_list.R
@@ -4,5 +4,9 @@ user <- read_csv("yelp_academic_dataset_user.csv")
 user$friend.list <- strsplit(user$friends,'\'')
 user$friend.list <- sapply(1:nrow(user), function(x) user$friend.list[[x]][c(F,T)])
 user$num_friends <- sapply(1:nrow(user), function(x) length(user$friend.list[[x]]))
+user$num_friends[is.na(user$friend.list)] <- 0
 
-edgelist <- cbind(user$user_id,user$friend.list)
+adjlist <- cbind(user$user_id,user$friend.list)
+
+library(igraph)
+
