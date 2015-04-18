7e79d8183e6229fda8c02f1c88d8558ef2e8a59d
jeffhudson <jdh2182@columbia.edu>
2015-04-17 20:29:04 -0400

    added creation of edgelist

--- extract_friend_list.R
+++ extract_friend_list.R
@@ -1,6 +1,8 @@
 library(readr)
 
 user <- read_csv("yelp_academic_dataset_user.csv")
-user$num_friends <- sapply(1:nrow(user), function(x) length(strsplit(user$friends[x],',')[[1]]))
 user$friend.list <- strsplit(user$friends,'\'')
 user$friend.list <- sapply(1:nrow(user), function(x) user$friend.list[[x]][c(F,T)])
+user$num_friends <- sapply(1:nrow(user), function(x) length(user$friend.list[[x]]))
+
+edgelist <- cbind(user$user_id,user$friend.list)
