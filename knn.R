e7cf05bf23136f83fbb6c8a7a53f4cebb164f74a
jeffhudson <jdh2182@columbia.edu>
2015-05-08 13:59:49 -0400

    adding updates to knn, matfact, and report

--- knn.R
+++ knn.R
@@ -3,31 +3,144 @@ library(Matrix)
 library(magrittr)
 library(dplyr)
 library(readr)
-
+# rm(list=ls())
 setwd("C:/Users/Jeff.Bernard/Data/Yelp/yelp_dataset_challenge_academic_dataset/")
 
 ##### SET UP #####
-reviews <- read_csv("no_text_reviews.csv")
-reviews$user_id %<>% as.factor
-reviews$business_id %<>% as.factor
+# reviews <- read_csv("no_text_reviews.csv")
+# reviews$user_id %<>% as.factor
+# reviews$business_id %<>% as.factor
+# 
+# business <- read_csv("yelp_academic_dataset_business.csv")
+# restaurants <- business[grep("Restaurant",business$categories),]
+# restreviews <- reviews[reviews$business_id %in% restaurants$business_id,] %<>% droplevels
+# 
+# write.csv(restreviews,"restaurantreviews.csv",row.names=F)
+# rm(list=ls())
+# 
+# reviews <- read_csv("restaurantreviews.csv")
+# reviews$user_id %<>% as.factor
+# reviews$business_id %<>% as.factor
+# 
+# business <- read_csv("yelp_academic_dataset_business.csv")
+# business$state %<>% as.factor
+# restaurants <- business[grep("Restaurant",business$categories),]
+# AZrests <- 
+#   dplyr::filter(restaurants,state=="AZ") %>% 
+#   dplyr::select(business_id) %>% 
+#   unlist
+# AZreviews <- 
+#   filter(reviews,business_id %in% AZrests) %>% 
+#   droplevels
+# 
+# AZmatrix <-  sparseMatrix(i=as.numeric(AZreviews$user_id),
+#                           j=as.numeric(AZreviews$business_id),
+#                           x=AZreviews$stars,
+#                           use.last.ij=TRUE)
+# numreviews <- rowSums(AZmatrix != 0)
+# 
+# active.users <- which(numreviews > 9)
+# 
+# act.user.reviews <- 
+#   AZreviews[as.numeric(AZreviews$user_id) %in% active.users,] %>% 
+#   dplyr::select(user_id,business_id,stars) %>%
+#   droplevels
 
-business <- read_csv("yelp_academic_dataset_business.csv")
-restaurants <- business[grep("Restaurant",business$categories),]
-restreviews <- reviews[reviews$business_id %in% restaurants$business_id,] %<>% droplevels
+########### start here ############
+act.user.reviews <- read_csv("actreviewmat.csv")
+act.user.reviews$business_id %<>% as.factor
+act.user.reviews$user_id %<>% as.factor
 
-write.csv(restreviews,"restaurantreviews.csv",row.names=F)
-rm(list=ls())
+act.review.mat <- sparseMatrix(i=as.numeric(act.user.reviews$user_id),
+                               j=as.numeric(act.user.reviews$business_id),
+                               x=act.user.reviews$stars,
+                               use.last.ij=TRUE)
 
-##### MAIN FUNCTION #####
+train <- read_csv("train.csv")
+test <- read_csv("test.csv")
 
-reviews <- read_csv("restaurantreviews.csv")
-reviews$user_id %<>% as.factor
-reviews$business_id %<>% as.factor
+userlist <- unique(train$user_id)
+
+test$pred3 <- 0
+test$pred5 <- 0
+test$pred7 <- 0
+test$pred9 <- 0
+test$pred11 <- 0
+
+# for each test point, 
+for(i in 1:nrow(test)){
+### find all users who rated that business, 
+  biz <- test[i,2] %>% as.character
+  user <- test[i,1] %>% as.character
+  stars <- train[train$business_id==biz,3] %>% unlist
+#  simusers <- train[train$business_id==biz,1] %>% unlist %>% c(user,.)
+#  usernums <- match(simusers,userlist)
+  print(paste(i,length(stars)))
+### find nearest k to test point (USING TRAIN DATA)
+#  simusermat <- act.review.mat[usernums,] %>% as.matrix
+#  d <- dist(simusermat, "euclidean") %>% as.matrix %>% .[1,-1]
+
+  simusers <- train[train$business_id==biz,1] %>% unlist
+  usernums <- match(simusers,userlist)
+  simusermat <- act.review.mat[usernums,] %>% as.matrix
+  d <- simusermat %>%
+    sweep(2,act.review.mat[match(user,userlist),]) %>%
+    .^2 %>% rowSums
+
+### take avg of points, then round
+  orderd <- stars[order(d)]
+  test$pred3[i] <- orderd %>% head(3) %>% mean %>% round
+  test$pred5[i] <- orderd %>% head(5) %>% mean %>% round
+  test$pred7[i] <- orderd %>% head(7) %>% mean %>% round
+  test$pred9[i] <- orderd %>% head(9) %>% mean %>% round
+  test$pred11[i] <- orderd %>% head(11) %>% mean %>% round
+}
+
+rmse3 <- (test$stars - test$pred3)^2 %>% na.omit %>% mean %>% sqrt
+rmse5 <- (test$stars - test$pred5)^2 %>% na.omit %>% mean %>% sqrt
+rmse7 <- (test$stars - test$pred7)^2 %>% na.omit %>% mean %>% sqrt
+rmse9 <- (test$stars - test$pred9)^2 %>% na.omit %>% mean %>% sqrt
+rmse11 <- (test$stars - test$pred11)^2 %>% na.omit %>% mean %>% sqrt
+
+##### could weight by distance (inverse)
+
+##### or network measures
 
 knn <- function(k){
   ## COMPUTE DISTANCES ##
   #for each business: compute distances between each user pair if not already computed
   # store results in sparse matrix
+  nusers <- act.user.reviews$user_id %>% unique %>% length
+  dists <- sparseMatrix(i=1:nusers,j=1:nusers,x=Inf,symmetric=TRUE,dims=c(nusers,nusers))
+  
+  ## reviews for businesses must be greater than 1
+  
+  for(bID in unique(act.user.reviews$business_id)){
+    revs <- act.user.reviews[act.user.reviews$business_id %in% bID,]
+    users <- unique(as.numeric(revs$user_id))
+    n <- length(users)
+    mat.sub <- act.review.mat[users,]
+    
+    # convert mat.sub to real matrix
+    mat.sub %<>% as.matrix
+    # run dist() on it
+    mdist <- dist(mat.sub) %>% as.matrix
+    # check all combos for non-zero values
+    udists <- dists[users,users] %>% as.matrix
+    zeroes <- udists == 0
+    # for all zero values, add dist to the matrix
+    
+#     for(i in seq_len(n-1)){
+#       eucd <- sqrt(rowSums((mat.sub[c(i+1):n,,drop=FALSE] - mat.sub[i,])^2))
+#       print(paste0("i",i))
+#       for(j in 1:length(eucd)){
+#         dists[users[i],users[i+j]] <- eucd[j]
+#         print(paste0("j",j))
+#       }
+#     }
+    print(which(unique(act.user.reviews$business_id)==bID))
+  }
+  
   review.matrix <- sparseMatrix(i=as.numeric(reviews$user_id),
                                 j=as.numeric(reviews$business_id),
                                 x=reviews$stars,
@@ -39,17 +152,7 @@ knn <- function(k){
   
 }
 
-business <- read_csv("yelp_academic_dataset_business.csv")
-business$state %<>% as.factor
-restaurants <- business[grep("Restaurant",business$categories),]
-PAlist <- filter(restaurants,state=="PA") %>% select(business_id) %>% unlist
-PArests <- filter(reviews,business_id %in% PAlist) %>% droplevels
-
-PAmatrix <-  sparseMatrix(i=as.numeric(PArests$user_id),
-                          j=as.numeric(PArests$business_id),
-                          x=PArests$stars,
-                          use.last.ij=TRUE)
-system.time(PAdists <- sparseDist(PAmatrix[1:5000,],7) %>% t)
+#system.time(AZdists <- sparseDist(PAmatrix[1:5000,],7) %>% t)
 
 
 numreviews <- rowSums(review.matrix != 0)
--- matfact.R
+++ matfact.R
@@ -56,5 +56,115 @@ for(i in 1:length(feasible)){
   st <- feasible[i]
   reclist[i] <- getLocalRec(st,"POPULAR",restaurants,restreviews)
 }
+library(MASS)
+MatrixFactorize <- function(train, test, sigsq=0.25, lambda=10, d=20, iterations=100){
+  
+  # define our prior belief
+  prior <- lambda * sigsq * diag(1, nrow=d, ncol=d) 
+  
+  # create sorted index list of unique users and movies in training set
+  # sorting is just convenience for when we need to calculate RMSE and objective function later
 
+  train <- act.user.reviews
+  
+  train$id <- paste0(train$user_id,train$business_id)
+  train <- train[!duplicated(train$id),-4]
+  
+  ndx <- sample(nrow(train),nrow(train)*0.1,FALSE)
+  
+  test <- train[ndx,]
+  train <- train[-ndx,]
+  
+  # remove test rows for which restaurant or user is not in training set
+  feasiblerows <- test$business_id %in% unique(train$business_id) & 
+    test$user_id %in% unique(train$user_id)
+  test <- test[feasiblerows,]
+  rm(feasiblerows)
+  
+  op <- rbind(train,test) %>% droplevels
+  
+  train <- op[1:nrow(train),]
+  test <- op[c(nrow(train)+1):nrow(op),]
+  
+  users <- as.character(unique(train$user_id))
+  rests <- as.character(unique(train$business_id))
+  
+  train <- arrange(train,business_id,user_id)
+  test <- arrange(test,business_id,user_id)
+
+  testrests_by_user <- sparseMatrix(i=as.numeric(test$user_id),
+                                    j=as.numeric(test$business_id),
+                                    x=T,
+                                    dims=c(length(users),length(rests))) %>% as.matrix
+    
+  # randomly initialize v
+  v <- mvrnorm(length(rests),rep(0,d),diag(1/lambda,d,d))
+  u <- matrix(0,nrow=length(users),ncol=d)
+  
+  # initialize Log likelihood and RMSE arrays
+  RMSE <- rep(0,iterations)
+  
+  for(step in 49:iterations){
+    
+    # update user locations
+    for(i in users){ 
+      #print(which(users %in% i))
+      omega <- train[which(train$user_id==i),-1]
+      restlist <- v[which(rests %in% omega$business_id),]
+      
+      p <- prior + (t(restlist) %*% restlist)
+      
+      u[which(users==i),] <- solve(p) %*% (t(restlist) %*% omega$stars) 
+    }
+    write.csv(u,paste0("u",step,".csv"),row.names=F)
+    print("finished u")          
+    # update movie locations
+    
+    for(j in rests){
+      omega <- train[which(train$business_id==j),-2]
+      uselist <- u[which(users %in% omega$user_id),]
+      
+      #print(which(rests %in% j))
+      
+      if(nrow(omega)==1){
+        p <- prior + (uselist %*% t(uselist))
+        v[which(rests==j),] <- solve(p) %*% (uselist * omega$stars)        
+      }else{
+        p <- prior + (t(uselist) %*% uselist)
+        v[which(rests==j),] <- solve(p) %*% (t(uselist) %*% omega$stars)
+      }
+    }
+    write.csv(v,paste0("v",step,".csv"),row.names=F)
+    print("finished v")
+
+    # predict all user-movie ratings
+    preds <- u %*% t(v)
+    
+
+    # calculate test predictions
+    tstpreds <- preds * testrests_by_user
+    tstpreds <- round(tstpreds[tstpreds != 0])
+    tstpreds[tstpreds > 5] <- 5
+    tstpreds[tstpreds < 1] <- 1
+  
+    # compute RMSE for this iteration (based on test set)
+    RMSE[step] <- sqrt(mean((test$stars - tstpreds)^2))
+    write.csv(RMSE,"rmse.csv",row.names=F)
+    print(paste("finished iteration", step))
+  }
+  return(list(L,RMSE,cbind(rests,v),u))
+}
+
+results <- MatrixFactorize(train, test)
+
+loss <- as.data.frame(cbind(x=1:100,y=results[[1]]))
+ggplot(loss, aes(x=x,y=y)) + geom_line() + 
+  labs(x="Iteration",y="Objective Function\n(Log Joint Likelihood)") + theme_bw()
+
+RMSE <- as.data.frame(cbind(x=1:100,y=results[[2]]))
+ggplot(RMSE, aes(x=x,y=y)) + geom_line() + labs(x="Iteration",y="RMSE") + theme_bw()
+
+v <- read.csv("v1.csv")
+
+u <- read.csv("u1.csv")
 
